5d238b98a182999800b99ba616776cf8383dac39
jagadeshwaran <jagsartchannel@gmail.com>
2026-01-15 16:51:32 +0530

    Added: cors dependency and logic

--- Mental-Health-Backend/app.js
+++ Mental-Health-Backend/app.js
@@ -6,10 +6,21 @@ const onboardingRoutes = require('./router/onboarding.router');
 const questionnaireRoutes = require('./router/questionnaire.router');
 const counsellorRoutes = require('./router/counsellor.router');
 const sessionRoutes = require('./router/session.router');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use(cors({
+    origin: [
+        'http://localhost:5173',  // Vite default
+        'http://localhost:8080'   // optional
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 
 app.use('/auth', authRoutes);
 app.use('/students', studentRoutes);
